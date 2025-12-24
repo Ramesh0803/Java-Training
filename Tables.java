ead00f65cc1e521ae642b0afa06fb47bd87ac8c5
Ramesh0803 <rameshryali748@gmail.com>
2025-12-24 10:19:56 +0530

    "Day-3" Loops

---
+++ Tables.java
@@ -0,0 +1,11 @@
+class Tables {
+    public static void main(String[] args) {
+
+        for (int no = 1; no <= 10; no = no + 1) {
+            for (int i = 1; i <= 10; i = i + 1) {
+                System.out.println(no + " x " + i + " = " + (no * i));
+            }
+            System.out.println();
+        }
+    }
+}
